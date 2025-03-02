b1e0593b5f48e75331ba2f85b795b176af0665ff
Manish Reddy <ysmanish5@gmail.com>
2025-03-02 16:01:05 -0700

    Added pid configs and stuff from phoenix tuner to code. Fixed incorrect canbus name in device constructors.

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -7,9 +7,11 @@ package frc.robot.subsystems;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.CANrange;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
+import com.ctre.phoenix6.signals.GravityTypeValue;
 import com.ctre.phoenix6.signals.InvertedValue;
 
 import dev.doglog.DogLog;
@@ -20,9 +22,9 @@ import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
-  private final TalonFX m_pivotMotor = new TalonFX(13, "canivore");
-  private final TalonFX m_rollerMotor = new TalonFX(14, "canivore");
-  private final CANrange intakeCANrange = new CANrange(20, "canivore");
+  private final TalonFX m_pivotMotor = new TalonFX(13, "CANivore");
+  private final TalonFX m_rollerMotor = new TalonFX(14, "CANivore");
+  private final CANrange intakeCANrange = new CANrange(20, "CANivore");
 
 
   /** Creates a new Intake. */
@@ -39,35 +41,46 @@ public class Intake extends SubsystemBase {
     var slot0Configs = pivotMotorConfigs.Slot0;
     slot0Configs.kG = 0.0; // 
     slot0Configs.kS = 0.0; // Add 0.25 V output to overcome static friction
-    slot0Configs.kV = 0.0; // A velocity target of 1 rps results in 0.12 V output
+    slot0Configs.kV = 0.46000000834465027; // A velocity target of 1 rps results in 0.12 V output
     slot0Configs.kA = 0.0; // An acceleration of 1 rps/s requires 0.01 V output
-    slot0Configs.kP = 0.0; // A positio n error of 2.5 rotations results in 12 V output
-    slot0Configs.kI = 0.0; // no output for integrated error
+    slot0Configs.kP = 20; // A positio n error of 2.5 rotations results in 12 V output
+    slot0Configs.kI = 0.00001; // no output for integrated error
     slot0Configs.kD = 0.0; // A velocity error of 1 rps results in 0.1 V output
+    slot0Configs.GravityType = GravityTypeValue.Arm_Cosine;
+
+    // need to 
+    // var positionVoltageCongigs = pivotMotorConfigs.
 
     // set Motion Magic settings
-    var motionMagicConfigs = pivotMotorConfigs.MotionMagic;
-    motionMagicConfigs.MotionMagicCruiseVelocity = 0.0; // Target cruise velocity of 80 rps
-    motionMagicConfigs.MotionMagicAcceleration = 0.0; // Target acceleration of 160 rps/s (0.5 seconds)
-    motionMagicConfigs.MotionMagicJerk = 0.0; // Target jerk of 1600 rps/s/s (0.1 seconds)
+    // var motionMagicConfigs = pivotMotorConfigs.MotionMagic;
+    // motionMagicConfigs.MotionMagicCruiseVelocity = 0.0; // Target cruise velocity of 80 rps
+    // motionMagicConfigs.MotionMagicAcceleration = 0.0; // Target acceleration of 160 rps/s (0.5 seconds)
+    // motionMagicConfigs.MotionMagicJerk = 0.0; // Target jerk of 1600 rps/s/s (0.1 seconds)
 
     m_pivotMotor.getConfigurator().apply(pivotMotorConfigs);
-    m_pivotMotor.setPosition(0.24707);
 
     
   }
 
+  // 0 for starting
+  // -0.243 for deployed
   // Intake Deploys at inputed angle
   public void goToAngle(double angle) {
-    final MotionMagicVoltage m_request = new MotionMagicVoltage(0);
+    // final MotionMagicVoltage m_request = new MotionMagicVoltage(0);
+    final PositionVoltage m_request = new PositionVoltage(angle).withSlot(0);
 
     m_pivotMotor.setControl(m_request.withPosition(angle));
+  }
+
+  public void goToDeployedPosition() {
+    
   }
 
   /* */
   public Command CommandGoToAngle(double angle) {
     return new InstantCommand(()-> goToAngle(angle), this);
   }
+  
 
   
   public void setRollerMotorPercentOutput(double outputPercent) {
