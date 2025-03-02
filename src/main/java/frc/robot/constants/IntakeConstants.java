71537b9b963aa8aed4ef986b0a5e8e12ee36c59c
Manish Reddy <ysmanish5@gmail.com>
2025-03-02 15:56:19 -0700

    Added deployed and undeployed angle values to constants

--- src/main/java/frc/robot/constants/IntakeConstants.java
+++ src/main/java/frc/robot/constants/IntakeConstants.java
@@ -2,6 +2,6 @@ package frc.robot.constants;
 
 public class IntakeConstants {
     public static final double deployAngle = 0;
-    public static final double framePerimeterAngle = 0;
+    public static final double framePerimeterAngle = -0.243;
 
 }
