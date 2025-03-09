ae1c105c9a8fb45225aad48fe434c371a8291d7d
IParsons1000 <163059817+IParsons1000@users.noreply.github.com>
2025-03-09 08:43:26 -0700

    Update Vision.java

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -60,7 +60,7 @@ public class Vision extends SubsystemBase {
     aprilCamLeft = new PhotonCamera(VisionConstants.kaprilCamLeftName);
     objectCam = new PhotonCamera(VisionConstants.kobjectCamName);
     photonEstimator =
-          new PhotonPoseEstimator(VisionConstants.kTagLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, VisionConstants.kRobotToRightAprilCam);
+          new PhotonPoseEstimator(VisionConstants.kTagLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, VisionConstants.kRobotToLeftAprilCam);
     photonEstimator.setMultiTagFallbackStrategy(PoseStrategy.LOWEST_AMBIGUITY);
 
         // ----- Simulation
@@ -80,7 +80,7 @@ public class Vision extends SubsystemBase {
             // targets.
             cameraSim = new PhotonCameraSim(aprilCamLeft, cameraProp);
             // Add the simulated camera to view the targets on this simulated field.
-            visionSim.addCamera(cameraSim, VisionConstants.kRobotToRightAprilCam);
+            visionSim.addCamera(cameraSim, VisionConstants.kRobotToLeftAprilCam);
 
             cameraSim.enableDrawWireframe(true);
       }
