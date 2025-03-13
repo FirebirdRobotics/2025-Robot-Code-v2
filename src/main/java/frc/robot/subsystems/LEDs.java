19a87d48accfa317ebc4c799dbc99ac25f682720
Manish Reddy <ysmanish5@gmail.com>
2025-03-12 21:07:05 -0700

    Created LED Colors, and blinking patterns, tried creating command to blink white and then turn solid

--- src/main/java/frc/robot/subsystems/LEDs.java
+++ src/main/java/frc/robot/subsystems/LEDs.java
@@ -4,15 +4,48 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.lib.trobot5013lib.led.BlinkingPattern;
+import frc.robot.lib.trobot5013lib.led.SolidColorPattern;
 import frc.robot.lib.trobot5013lib.led.TrobotAddressableLED;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 
 public class LEDs extends SubsystemBase {
   int ledLength = 90;
-  int ledPWMPort = 3;
+  int ledPWMPort = 9;
   
   TrobotAddressableLED m_ledStrip = new TrobotAddressableLED(ledPWMPort, ledLength);
   
+  Color white = new Color("fdf0d5");
+  Color red = new Color("c1121f");
+  Color green = new Color("a7c957");
+  Color black = new Color(1,1,1); // Not sure if this will work
+
+  BlinkingPattern blinkingWhite = new BlinkingPattern(white, 0.2);
+  BlinkingPattern blinkingRed = new BlinkingPattern(red, 0.2);
+  BlinkingPattern blinkingGreen = new BlinkingPattern(green, 0.2);
+  BlinkingPattern blinkingBlack = new BlinkingPattern(black, 0.2);
+
+  SolidColorPattern solidWhite = new SolidColorPattern(white);
+  SolidColorPattern solidRed = new SolidColorPattern(red);
+  SolidColorPattern solidGreen = new SolidColorPattern(green);
+  SolidColorPattern solidBlack = new SolidColorPattern(black);
+
+
+
+
+  public Command blinkWhiteThenStayWhite() {
+    return runEnd(
+      () -> m_ledStrip.setPattern(blinkingWhite),
+      () -> m_ledStrip.setPattern(solidWhite)
+    ); 
+ 
+  }
+
   /** Creates a new LEDs. */
   public LEDs() {}
 
