c992ee490fa0002adceefac39522e45bb7d573ea
Manish Reddy <ysmanish5@gmail.com>
2025-03-14 11:31:08 -0700

    made L4 height max height

--- src/main/java/frc/robot/constants/ElevatorConstants.java
+++ src/main/java/frc/robot/constants/ElevatorConstants.java
@@ -5,7 +5,7 @@ public class ElevatorConstants {
     public static final int elevatorFollowerMotorID = 48;
     public static final int elevatorCANcoderID = 60;
 
-    public static final double L4height = 23.39111328125;
+    public static final double L4height = 24.09228515625;
     public static final double L3height = 14.962646484375;
     public static final double L2height = 10.677;
     public static final double L1height = 4.5;
