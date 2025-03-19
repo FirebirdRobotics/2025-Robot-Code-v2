d592734a97e944b414a79a58c430f3c68d80f82c
Manish Reddy <ysmanish5@gmail.com>
2025-03-19 13:44:01 -0700

    Add current limits to roller motor and pivot motor, made the method goToDeployedPosition also set the roller speed to 80% and the method  goToFramePerimeterPosition to set the roller speed to 0%

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -16,11 +16,14 @@ import com.ctre.phoenix6.signals.GravityTypeValue;
 import com.ctre.phoenix6.signals.InvertedValue;
 
 import dev.doglog.DogLog;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.constants.IntakeConstants;
 
 public class Intake extends SubsystemBase {
@@ -31,6 +34,7 @@ public class Intake extends SubsystemBase {
 
   /** Creates a new Intake. */
   public Intake() {
+    
     var pivotMotorConfigs = new TalonFXConfiguration();
 
     var intakeCANRangeConfigs = new CANrangeConfiguration();
@@ -40,6 +44,15 @@ public class Intake extends SubsystemBase {
     pivotMotorConfigs.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
     pivotMotorConfigs.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RotorSensor;
     pivotMotorConfigs.Feedback.SensorToMechanismRatio = 23.265306122;
+    pivotMotorConfigs.CurrentLimits.SupplyCurrentLimit = 60;
+    pivotMotorConfigs.CurrentLimits.StatorCurrentLimit = 60;
+
+    pivotMotorConfigs.CurrentLimits.StatorCurrentLimitEnable = true;
+    pivotMotorConfigs.CurrentLimits.StatorCurrentLimit = 50;
+
+    pivotMotorConfigs.CurrentLimits.SupplyCurrentLimitEnable = true;
+    pivotMotorConfigs.CurrentLimits.SupplyCurrentLimit = 50;
+
     
 
 
@@ -53,6 +66,19 @@ public class Intake extends SubsystemBase {
     slot0Configs.kI = 0.00001; // no output for integrated error
     slot0Configs.kD = 0.0; // A velocity error of 1 rps results in 0.1 V output
     slot0Configs.GravityType = GravityTypeValue.Arm_Cosine;
+    
+
+    // var slot1Configs = pivotMotorConfigs.Slot1;
+    // slot1Configs.kG = 0.0; // 
+    // slot1Configs.kS = 0.0; // Add 0.25 V output to overcome static friction
+    // slot1Configs.kV = 0.0; // A velocity target of 1 rps results in 0.12 V output
+    // slot1Configs.kA = 0.0; // An acceleration of 1 rps/s requires 0.01 V output
+    // slot1Configs.kP = 0.0; // A positio n error of 2.5 rotations results in 12 V output
+    // slot1Configs.kI = 0.0; // no output for integrated error
+    // slot1Configs.kD = 0.0; // A velocity error of 1 rps results in 0.1 V output
+    // slot1Configs.GravityType = GravityTypeValue.Arm_Cosine;
+
+
 
     // need to 
     // var positionVoltageCongigs = pivotMotorConfigs.
@@ -64,6 +90,17 @@ public class Intake extends SubsystemBase {
     motionMagicConfigs.MotionMagicJerk = 0.0; // Target jerk of 1600 rps/s/s (0.1 seconds)
 
     m_pivotMotor.getConfigurator().apply(pivotMotorConfigs);
+
+    var intakeRollerConfigs = new TalonFXConfiguration();
+    intakeRollerConfigs.CurrentLimits.StatorCurrentLimitEnable = true;
+    intakeRollerConfigs.CurrentLimits.StatorCurrentLimit = 50;
+
+    intakeRollerConfigs.CurrentLimits.SupplyCurrentLimitEnable = true;
+    intakeRollerConfigs.CurrentLimits.SupplyCurrentLimit = 50;
+
+    m_rollerMotor.getConfigurator().apply(intakeRollerConfigs);
+
+
 
     
   }
@@ -78,13 +115,16 @@ public class Intake extends SubsystemBase {
     m_pivotMotor.setControl(m_request.withPosition(angle));
   }
 
-  public Command testIntakeDeployAndUndeploy() {
+  public Command goToDeployAndThenToUndeployCommand() {
     return runEnd(
       () -> goToDeployedPosition(),
       () -> goToFramePerimeterPosition()
       
       ); 
   }
+
+
+  
 
   public void goToDeployedPosition() {
     // final PositionVoltage m_request = new PositionVoltage(IntakeConstants.deployAngle).withSlot(0);
@@ -95,14 +135,58 @@ public class Intake extends SubsystemBase {
 
     m_pivotMotor.setControl(m_request.withPosition(IntakeConstants.deployAngle));
 
+    setRollerMotorPercentOutput(0.8);
+
   }
+
+  /*The following code that was commented out was initially intended to be used to make 
+  the intake free of any motor power trying to make it rigid on the deployed position
+   -- we ended up not using it because we got the intake working without
+    it needing to passive/floppy in the deployed position */
+    
+  // public void setCurrentLimit0() {
+  //   var pivotMotorConfigs = new TalonFXConfiguration();
+
+  //   pivotMotorConfigs.CurrentLimits.StatorCurrentLimit = 1;
+
+  //   pivotMotorConfigs.CurrentLimits.SupplyCurrentLimit = 1;    
+
+  //   m_pivotMotor.getConfigurator().apply(pivotMotorConfigs);
+
+  // }
+
+  // public void setCurrentLimitNormal() {
+  //   var pivotMotorConfigs = new TalonFXConfiguration();
+
+  //   pivotMotorConfigs.CurrentLimits.StatorCurrentLimit = 120;
+
+  //   pivotMotorConfigs.CurrentLimits.SupplyCurrentLimit = 120;
+
+  //   m_pivotMotor.getConfigurator().apply(pivotMotorConfigs);
+
+  // }
+
+  // public Command setCurrentLimit0AndThenNormalRunEndCommand() {
+  //   return runEnd(
+  //     () -> setCurrentLimit0(),
+  //     () -> setCurrentLimitNormal()
+  //   ); 
+  // }
+
+   
 
   public void goToFramePerimeterPosition() {
     final MotionMagicVoltage m_request = new MotionMagicVoltage(IntakeConstants.framePerimeterAngle);
 
     m_pivotMotor.setControl(m_request.withPosition(IntakeConstants.framePerimeterAngle));
 
+    setRollerMotorPercentOutput(0);
   }
+
+  // public Command goToFramePerimeterPositionCommand() {
+  //   return new InstantCommand(()-> goToAngle(IntakeConstants.framePerimeterAngle), this);
+  // }
+
 
   /* */
   public Command CommandGoToAngle(double angle) {
@@ -110,19 +194,104 @@ public class Intake extends SubsystemBase {
   }
   
 
+  // public void setAllClosedLoopConfigsTo0() {
+  //   var pivotMotorConfigs = new TalonFXConfiguration();
+
+  //   pivotMotorConfigs.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+  //   pivotMotorConfigs.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RotorSensor;
+  //   pivotMotorConfigs.Feedback.SensorToMechanismRatio = 23.265306122;
+    
+
+
+  //   // set slot 0 gains
+  //   var slot0Configs = pivotMotorConfigs.Slot0;
+  //   slot0Configs.kG = 0.0; // 
+  //   slot0Configs.kS = 0.0; // Add 0.25 V output to overcome static friction
+  //   slot0Configs.kV = 0.0; // A velocity target of 1 rps results in 0.12 V output
+  //   slot0Configs.kA = 0.0; // An acceleration of 1 rps/s requires 0.01 V output
+  //   slot0Configs.kP = 0; // A positio n error of 2.5 rotations results in 12 V output
+  //   slot0Configs.kI = 0; // no output for integrated error
+  //   slot0Configs.kD = 0.0; // A velocity error of 1 rps results in 0.1 V output
+  //   slot0Configs.GravityType = GravityTypeValue.Arm_Cosine;
+
+  //   // need to 
+  //   // var positionVoltageCongigs = pivotMotorConfigs.
+
+  //   // set Motion Magic settings
+  //   var motionMagicConfigs = pivotMotorConfigs.MotionMagic;
+  //   motionMagicConfigs.MotionMagicCruiseVelocity = 30.0; // Target cruise velocity of 80 rps
+  //   motionMagicConfigs.MotionMagicAcceleration = 30.0; // Target acceleration of 160 rps/s (0.5 seconds)
+  //   motionMagicConfigs.MotionMagicJerk = 0.0; // Target jerk of 1600 rps/s/s (0.1 seconds)
+
+  //   m_pivotMotor.getConfigurator().apply(pivotMotorConfigs);
+
+  // }
+
+  // public void setAllClosedLoopConfigsToCorrectValues() {
+  //   var pivotMotorConfigs = new TalonFXConfiguration();
+
+  //   // set Motion Magic settings
+  //   var motionMagicConfigs = pivotMotorConfigs.MotionMagic;
+  //   motionMagicConfigs.MotionMagicCruiseVelocity = 5.0; // Target cruise velocity of 80 rps
+  //   motionMagicConfigs.MotionMagicAcceleration = 5.0; // Target acceleration of 160 rps/s (0.5 seconds)
+  //   motionMagicConfigs.MotionMagicJerk = 0.0; // Target jerk of 1600 rps/s/s (0.1 seconds)
+
+
+  //   // set slot 0 gains
+  //   var slot0Configs = pivotMotorConfigs.Slot0;
+  //   slot0Configs.kG = 0.0; // 
+  //   slot0Configs.kS = 0.0; // Add 0.25 V output to overcome static friction
+  //   slot0Configs.kV = 0.46000000834465027; // A velocity target of 1 rps results in 0.12 V output
+  //   slot0Configs.kA = 0.0; // An acceleration of 1 rps/s requires 0.01 V output
+  //   slot0Configs.kP = 20; // A positio n error of 2.5 rotations results in 12 V output
+  //   slot0Configs.kI = 0.00001; // no output for integrated error
+  //   slot0Configs.kD = 0.0; // A velocity error of 1 rps results in 0.1 V output
+  //   slot0Configs.GravityType = GravityTypeValue.Arm_Cosine;
+
+  //   // need to 
+  //   // var positionVoltageCongigs = pivotMotorConfigs.
+
+
+  //   m_pivotMotor.getConfigurator().apply(pivotMotorConfigs);
+
+
+  // }
+
+
   
   public void setRollerMotorPercentOutput(double outputPercent) {
     m_rollerMotor.setControl(new DutyCycleOut(outputPercent));
   }
 
-  public Command setRollerMotorPercentOutputCommand(double power) {
+  public Command setRollerMotorPercentOutputAndThenTo0Command(double power) {
     
     return runEnd(
-      () -> setRollerMotorPercentOutput(0.442),
+      () -> setRollerMotorPercentOutput(power),
       () -> setRollerMotorPercentOutput(0)
     ); 
 
   }
+
+  public Command setRollerMotorPercentOutputCommand(double power) {
+    return runOnce(() -> setRollerMotorPercentOutput(power));
+    
+
+  }
+
+  public Command goToDeployedPositionCommand() {
+    return runOnce(() -> goToDeployedPosition());
+    
+  }
+
+  public Command goToFramePerimeterPositionCommand() {
+    return runOnce(() -> goToFramePerimeterPosition());
+    
+  }
+
+
+  
+
+  
 
 
   // public boolean motorHitHardstop() {
@@ -146,13 +315,18 @@ public class Intake extends SubsystemBase {
   //   return new ParallelDeadlineGroup(null, null)
   // }
 
+
+
   @Override
   public void periodic() {
+    
     // This method will be called once per scheduler run
     // SmartDashboard.putNumber("IntakeCANRangeDistance", intakeCANrange.getDistance().getValueAsDouble());
     // SmartDashboard.putNumber("Intake Pivot Angle", m_pivotMotor.getPosition().getValueAsDouble());
     DogLog.log("Intake/IntakeCANrangeDistance", intakeCANrange.getDistance().getValueAsDouble());
     DogLog.log("Intake/IntakeCANrangeBoolean", intakeCANrange.getIsDetected().getValue());
     DogLog.log("Intake/IntakePivotAngle", m_pivotMotor.getPosition().getValueAsDouble());
+
+    
   }
 }
