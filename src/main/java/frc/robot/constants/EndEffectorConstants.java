cbd78faa52dac3c4a97fddbdcb3ceb76963b2c28
Manish Reddy <ysmanish5@gmail.com>
2025-03-20 13:50:35 -0700

    after calibration

--- src/main/java/frc/robot/constants/EndEffectorConstants.java
+++ src/main/java/frc/robot/constants/EndEffectorConstants.java
@@ -1,9 +1,13 @@
 package frc.robot.constants;
 
 public class EndEffectorConstants {
+    public static final double humanFeedAngle = 0.05126953125;
+
     public static final double L4Angle = -0.17626953125;
-    public static final double L3Angle = -0.142;
-    public static final double L2Angle = -0.142;
+    // -0.127197265625
+    
+    public static final double L3Angle = -0.13916015625;
+    public static final double L2Angle = -0.1416015625;
     public static final double L1Angle = -0.0048828125;
 
     public static final double stowedAngle = -0.07080078125;
